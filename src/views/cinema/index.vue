4149a050becfa49c53dd651d6af232c6460784e3
zhuhaolin <18575624813@163.com>
2020-06-14 13:00:13 +0800

    基本页面开发

---
+++ src/views/cinema/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="main">
+        <top title="瞄瞄影院"></top>
+        <div id="content">
+            <div class="cinema_menu">
+                <div class="city_switch">
+                    全城 <i class="iconfont icon-lower-triangle"></i>
+                </div>
+                <div class="brand_swtich">
+                    品牌 <i class="iconfont icon-lower-triangle"></i>
+                </div>
+                <div class="feature_switch">
+                    特色 <i class="iconfont icon-lower-triangle"></i>
+                </div>
+            </div>
+        </div>
+        <cilist></cilist>
+        <bottom></bottom>
+    </div>
+</template>
+
+<script>
+    import top from '@/components/header';
+    import bottom from '@/components/tabbar';
+    import cilist from '@/components/cilist';
+
+    export default {
+        name: "cinema",
+        components: {
+            top,
+            bottom,
+            cilist
+        },
+        data() {
+            return {};
+        },
+        watch: {},
+        created() {
+        },
+        methods: {},
+        computed: {}
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
